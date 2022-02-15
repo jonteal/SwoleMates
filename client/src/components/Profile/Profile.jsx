f9a2af09172b2bf74dc5e79744781b208cbdd550
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-14 19:37:34 -0700

    allow user to upload file

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -21,6 +21,12 @@ class Profile extends React.Component {
     
       render() {
         return (
+        <>
+            Welcome, (future username here), please complete your profile to meet your future swole mate 💓
+
+            <input type="file" />
+
+
           <form onSubmit={this.handleSubmit}>
             <label>
               First Name:
@@ -34,14 +40,16 @@ class Profile extends React.Component {
             <select>
                 <option value="1.2"> Sedentary (little to no exercise)</option>
                 <option value="1.375">Lightly Active (light exercise/sports 1-3 days/week)</option>
-                <option selected value="1.55">Moderately active (moderate exercise/sports 3-5 days/week)</option>
-                <option value="1.725">Very active (hard exercise/sports 6-7 days a week)</option>
-                <option value="1.9">Extra active (very hard exercise/sports & physical job or 2x training)</option>
+                <option selected value="1.55">Moderately Active (moderate exercise/sports 3-5 days/week)</option>
+                <option value="1.725">Very Active (hard exercise/sports 6-7 days a week)</option>
+                <option value="1.9">Extra Active (very hard exercise/sports & physical job or 2x training)</option>
             </select>
 
             <input type="submit" value="Submit" />
           </form>
+          </>
         );
+        
       }
 }
 
