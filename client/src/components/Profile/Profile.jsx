5370a869acdd50db414fed6f9aa8fb9441e490c6
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-14 19:34:22 -0700

    Created drop down menu for activity levels

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -23,9 +23,22 @@ class Profile extends React.Component {
         return (
           <form onSubmit={this.handleSubmit}>
             <label>
-              Name:
+              First Name:
               <input type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
+            <label>
+              Last Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+
+            <select>
+                <option value="1.2"> Sedentary (little to no exercise)</option>
+                <option value="1.375">Lightly Active (light exercise/sports 1-3 days/week)</option>
+                <option selected value="1.55">Moderately active (moderate exercise/sports 3-5 days/week)</option>
+                <option value="1.725">Very active (hard exercise/sports 6-7 days a week)</option>
+                <option value="1.9">Extra active (very hard exercise/sports & physical job or 2x training)</option>
+            </select>
+
             <input type="submit" value="Submit" />
           </form>
         );
