05250acf65a73afb307bcf2950a3ef73bc9d8b6f
Valerii Bihun <valeriibihun.co@gmail.com>
2022-02-15 23:46:47 -0700

    Comments added

--- client/src/components/MealPlanner/MealPlanner.jsx
+++ client/src/components/MealPlanner/MealPlanner.jsx
@@ -2,24 +2,26 @@ import React, { useState, useEffect } from "react";
 const API_KEY = "?apiKey=f8a19463536b4ffb8c05cdb882afb0c8";
 
 const MealPlanner = () => {
+  //   Setting up a state to save fetched data:
   const [foodPlan, setPlan] = useState([]);
 
-  // cal is calculated based on our BMR;
-
+  //   Calories are calculated based on our BMR;
   var calories = 2000;
 
+  //   Initiating data fetch from Spunacular API:
   useEffect(() => {
     fetchMealPlan(calories);
   }, []);
 
+  //   Data fetch:
   const fetchMealPlan = (calories) => {
     let fetchMealPlanUrl = `https://api.spoonacular.com/mealplanner/generate${API_KEY}&time=day&targetCalories=${calories}`;
     fetch(fetchMealPlanUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.week);
-        console.log(data.week.length);
-        console.log(data.week.monday.meals[0].sourceUrl);
+        // console.log(data.week);
+        // console.log(data.week.length);
+        // console.log(data.week.monday.meals[0].sourceUrl);
         setPlan(data.week);
       });
   };
