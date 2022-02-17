d6472c4c63fcbf9931e0841b5f6862829dd9a05f
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-16 22:46:40 -0600

    Deleted Bootstrap import

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect } from "react";
 import "./LoginForm.css";
-import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
