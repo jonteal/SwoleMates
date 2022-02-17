de7b4aa74e17d40bb842ee3d004044ce0d918413
Eric Martin <eric_martin@me.com>
2022-02-17 15:13:17 -0700

    created dashboard and updated components and css

---
+++ client/src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import ComingSoon from './components/ComingSoon';
+import Dashboard from './components/Dashboard/Dashboard';
+import Navbar from './components/Navbar/Navbar';
+import MealPlanner from './components/MealPlanner/MealPlanner.jsx'
+import Profile from './components/Profile/Profile'
+import Foodbar from './components/Food/Food'
+import Quote from './components/Quote/Quote';
+import Exercise from './components/Tracker/Exercise';
+import PersonalDevelopment from './components/PersonalDevelopment/PersonalDevelopment';
+
+
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import SignupForm from './components/SignupForm/SignupForm';
+import LoginForm from './components/LoginForm/LoginForm';
+import Weight from './components/Weight/Weight';
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <>
+        <Router>
+
+          <Navbar />
+
+          <Switch>
+            <Route exact path='/' component={LoginForm} />
+            <Route exact path='/signup' component={SignupForm} />
+            <Route exact path='/home' component={Dashboard} />
+            <Route exact path='/profile' component={Profile} />
+            <Route exact path='/quote' component={Quote} />
+            <Route exact path='/weight' component={Weight} />
+            <Route exact path='/personaldevelopment' component={PersonalDevelopment} />
+            <Route exact path='/food' component={Foodbar} />
+            <Route exact path='/mealplan' component={MealPlanner} />
+            <Route exact path='/exercise' component={Exercise} />
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </Router>
+      </>
+    </ApolloProvider>
+
+  );
+}
+
+export default App;
---
+++ client/src/components/PersonalDevelopment/PersonalDevelopment.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import './PersonalDevelopment.css';
+
+const PersonalDevelopment = () => {
+
+    const [text, setText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const body = { text }
+
+        // Can we use a POST fetch request and post this user input to our "HomePage"?
+        // Location of fetch request can be where it will populate on homepage. Something like 
+        // 'http://localhost:8000/goals' or something
+        fetch('', {         
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+        }).then(() => {
+            console.log('new personal development goal added!')
+        })
+
+    }
+    return (
+        <div className="dev-container filter drop-shadow-lg">
+            <form className="dev-form" onSubmit={handleSubmit}>
+                <textarea
+                    className="dev-input"
+                    rows="2"
+                    cols="30"
+                    placeholder="What is your WHY?"
+                    type="text"
+                    required
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button className="dev-btn filter drop-shadow-lg">
+                    Save
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default PersonalDevelopment;
