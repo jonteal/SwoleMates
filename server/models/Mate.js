69fe5a2b53261dc3c5bcf9fe17f7534f3ad16c08
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-16 21:07:44 -0600

    Mate model rebuilt after messing up and creating it on the main

--- server/models/Mate.js
+++ server/models/Mate.js
@@ -1,1 +1,10 @@
 const { model, Schema } = require('mongoose');
+
+const mateSchema = new Schema({
+    firstName: String,
+    password: String,
+    email: String,
+    createdAt: String
+});
+
+module.exports = model('Mate', mateSchema);
