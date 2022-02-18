c434f8f50e65b84cb12ca8756da5cc8275a62bb7
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-17 20:59:04 -0700

    Centered login button

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -17,7 +17,7 @@ const Welcome = () => {
     return (
         <>
         {!openModal && 
-        <div>
+        <div className="welcomeContainer">
             <div className="container">
                 <div className="shadows">
                     <span>S</span>
@@ -34,27 +34,28 @@ const Welcome = () => {
             </div>
     
 
-                <div className="welcomeBorder bg-red-500">
-                Are you ready to find your one true swolemate?
-                SCROLL DOWN
+                <div className="welcomeBorder">
+
+                <div class="relative rounded-xl overflow-auto p-8">
+                        <div class="flex justify-center items-end">
+                        <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+                        <svg className="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                        </svg>
+                        </div>
+                        </div>
+                        </div>
+
             </div>
 
             
-                <div className="bg-white btnCard">
+                <div className="bg-blue-900 btnCard">
+
                     <button onClick={() => {setModal(true)}} value={openModal} className="openModalBtn">
                        Click here to login, mate
-                        </button>
-                    <br />
+                    </button>
 
-                        <div class="relative rounded-xl overflow-auto p-8">
-                <div class="flex justify-center items-end">
-                    <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
-                    <svg className="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                    </svg>
-                    </div>
-                </div>
-                </div>
+                    <br />
             </div>
         
         </div>}
