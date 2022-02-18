6664955a3fd03ca8b91cf056f9a5dfb9b5826bdf
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-18 09:55:31 -0700

    Changing color theme for btn and sweat

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -55,7 +55,7 @@ return (
         <div className="min-h-screen max-h-screen max-w-screen flex justify-center items-center loginBg">
             <div className="w-1/3 h-1/2 loginCard">
                 
-            <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#8ceabb"/><stop offset="100%" stop-color="#378f7b"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#76D9F0"/><stop offset="100%" stop-color="#096479"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
                 
                 
                 <h2 className="text-3x1 font-bold mb-10 text-center font-fa loginTitle">LOG IN</h2>
