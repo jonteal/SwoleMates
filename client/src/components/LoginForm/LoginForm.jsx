78fe227f60590bc5ea37c2ba6bba61697df3f52c
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-18 09:47:45 -0700

    Styling login card, added box shadow

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,11 +1,10 @@
 import React, {useState, useEffect } from "react";
 import "./loginForm.css";
-
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 import SignupForm from '../SignupForm/SignupForm';
-
+import { Link } from 'react-router-dom'
 import {handleModal} from '../Welcome/Welcome'
 
 const LoginForm = ({handleModal}) => {
@@ -54,7 +53,7 @@ const LoginForm = ({handleModal}) => {
 return (
     <>
         <div className="min-h-screen max-h-screen max-w-screen flex justify-center items-center loginBg">
-            <div className="p-16 rounded shadow-2xl w-1/3 h-1/2 loginCard">
+            <div className="w-1/3 h-1/2 loginCard">
                 
             <svg xmlns="http://www.w3.org/2000/svg" class="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#8ceabb"/><stop offset="100%" stop-color="#378f7b"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
                 
@@ -65,10 +64,10 @@ return (
                     Something went wrong with your login credentials!
                     </alert> */}
                     <div>
-                    <label className="block mb-2 font-bold" htmlFor='email'>Email</label>
+                    <label className="block mb-2 font-bold" htmlFor='email'></label>
                     <input
                         type='text'
-                        placeholder='Your email'
+                        placeholder='Email'
                         name='email'
                         onChange={handleInputChange}
                         value={userFormData.email}
@@ -78,10 +77,10 @@ return (
                     </div>
 
                     <div>
-                    <label className="block mb-2 font-bold" htmlFor='password'>Password</label>
+                    <label className="block mb-2 font-bold" htmlFor='password'></label>
                     <input
                         type='password'
-                        placeholder='Your password'
+                        placeholder='Password'
                         name='password'
                         onChange={handleInputChange}
                         value={userFormData.password}
@@ -95,7 +94,7 @@ return (
                         variant='success' className="loginBtn">
                         Log In
                     </button>
-                    <p className="loginSignup">Don't have an account? <span><a href={SignupForm} className="underline cursor-pointer">Sign up!</a></span></p>
+                    <p className="loginSignup">Don't have an account? <span><Link to="/signup" className="underline cursor-pointer">Sign up!</Link></span></p>
                 </form>
             </div>
 
