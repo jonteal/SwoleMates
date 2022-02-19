4fed7f750b8f9aba9abc35941e750329c22106ce
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-19 09:43:00 -0600

    formatting queries

--- client/src/components/Mates/Mates.js
+++ client/src/components/Mates/Mates.js
@@ -1,13 +1,19 @@
 import React from "react";
 import './Mates.css';
 
-import { useMutation } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 
 import {
     FETCH_POSTS_QUERY
 } from "../../utils/queries";
 
 const Mates = () => {
+    const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+    if (data) {
+        console.log(data);
+    }
+
     return (
         <h1>Mates</h1>
         
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,14 +4,23 @@ import { gql } from '@apollo/client';
 
 
 export const FETCH_POSTS_QUERY = gql`
+{
     getPosts{
-    id body createdAt firstName likeCount
-    likes{
-    firstName
+    id 
+    body 
+    createdAt 
+    firstName 
+    likeCount
+    likes {
+        firstName
     }
     commentCount
-    comments{
-    id firstName createdAt body
+    comments {
+        id 
+        firstName 
+        createdAt 
+        body
     }
+}
 }
 `
