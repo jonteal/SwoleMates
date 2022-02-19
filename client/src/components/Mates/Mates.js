a5b86cffc987f8a23a25d052521d3c8d4c75e5fc
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-19 08:14:26 -0600

    started fetch posts query

--- client/src/components/Mates/Mates.js
+++ client/src/components/Mates/Mates.js
@@ -1,4 +1,5 @@
 import React from "react";
+import gql from 'graphql';
 import './Mates.css';
 
 const Mates = () => {
@@ -7,5 +8,18 @@ const Mates = () => {
         
     )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    getPosts{
+    id body createdAt firstName likeCount
+    likes{
+    firstName
+    }
+    commentCount
+    comments{
+    id firstName createdAt body
+    }
+}
+`
 
 export default Mates;
