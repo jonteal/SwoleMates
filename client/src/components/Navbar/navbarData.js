6c0c4858cfc4baaef8b58e62a3699b9444a438db
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-19 08:20:04 -0700

    Fixed Navbar folder name issue

---
+++ client/src/components/Navbar/navbarData.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as IoIcons from 'react-icons/io';
+import * as MdIcons from 'react-icons/md';
+import * as FaIcons from 'react-icons/fa';
+
+
+export const NavbarData = [
+    {
+        title: 'Home',
+        path: '/home',
+        icon: <FaIcons.FaHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Food',
+        path: '/food',
+        icon: <MdIcons.MdFastfood />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Exercise',
+        path: '/exercise',
+        icon: <IoIcons.IoIosFitness />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Mates',
+        path: '/mates',
+        icon: <FaIcons.FaUserFriends />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Donate',
+        path: '/donate',
+        icon: <MdIcons.MdOutlineAttachMoney />,
+        cName: 'nav-text'
+    },
+]
