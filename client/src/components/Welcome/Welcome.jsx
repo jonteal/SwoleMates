5733ac8bbfe801007232b7360326c72e269b1ef5
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-19 12:38:45 -0700

    Styling welcome page, removed all white bg and button

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Modal from "../LoginForm/LoginForm";
+import Login from "../LoginForm/LoginForm";
 import "./welcome.css";
 import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 
 const Welcome = () => {
   //represents whether the modal is open or not, start false bc modal closed initially
-  const [openModal, setModal] = useState(false);
+  const [openLogin, setLogin] = useState(false);
   const [scrollTop, setScrollTop] = useState();
   const [scrolling, setScrolling] = useState();
   useEffect(() => {
@@ -24,7 +24,7 @@ const Welcome = () => {
   return (
     <>
       <div className="welcomeMain">
-        {!openModal && (
+        {!openLogin && (
           <div className="welcomeContainer">
             <div className="container">
               <div className="shadows">
@@ -43,7 +43,9 @@ const Welcome = () => {
 
             <div>
               <div className="flex justify-center items-end">
-                <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
+                <div className="animate-bounce p-2 w-10 h-10 ring-1 ring-black shadow-lg rounded-full flex items-center justify-center cursor-pointer bounceArrow" onClick={() => {
+                setLogin(true);
+              }}>
                   <svg
                     className="w-6 h-6 text-violet-500"
                     fill="none"
@@ -56,25 +58,15 @@ const Welcome = () => {
                     <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
                   </svg>
                 </div>
+                
               </div>
             </div>
-
-            <button
-              onClick={() => {
-                setModal(true);
-              }}
-              value={openModal}
-              className="button-57"
-              role="button"
-            >
-              Ready?<span>Let's go...</span>
-            </button>
 
             <br />
           </div>
         )}
 
-        {openModal && (
+        {openLogin && (
           <CSSTransitionGroup
             transitionName="bottomToTop"
             transitionAppear={true}
@@ -82,7 +74,7 @@ const Welcome = () => {
             transitionEnter={false}
             transitionLeave={false}
           >
-            <Modal onScroll={(event) => setScrolling(event)} />
+            <Login onScroll={(event) => setScrolling(event)} />
           </CSSTransitionGroup>
         )}
       </div>
