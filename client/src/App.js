93b20c016c81298888161e051ce4570cd7411072
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-19 07:39:46 -0600

    imported semantic-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import 'semantic-ui-css/semantic.min.css';
 import ComingSoon from './components/ComingSoon';
 
 // import Navbar from './components/navbar/Navbar';
