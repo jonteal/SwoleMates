9cbfe13e8ef1b1c794688e7a0d0767909abf4cdf
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 06:52:38 -0700

    Create globalstate, opted to not go redux route at this time

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -129,3 +129,21 @@ export const UPDATE_WEIGHT = gql`
     }
   }
 `;
+
+export const ADD_ORDER = gql`
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchaseDate
+      products {
+        _id
+        name
+        description
+        price
+        quantity
+        category {
+          name
+        }
+      }
+    }
+  }
+`;
