9897f03d99cd02cd95675074ecd9e31f0f280359
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 07:29:41 -0700

    Entered seeds, test run passed

--- client/src/components/Sponsor/Sponsor.jsx
+++ client/src/components/Sponsor/Sponsor.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ProductList from "../ProductList/ProductList";
 import Cart from "../Cart/Cart";
-import CategoryMenu from "../CategoryMenu/CategoryMenu";
+
 
 const Sponsor = () => {
   return (
     <div className="container">
-      <CategoryMenu />
+
       <ProductList />
       <Cart />
     </div>
