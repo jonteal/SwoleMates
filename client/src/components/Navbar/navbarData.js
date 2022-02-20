2fc2192a8947b81e09add825ec694f0ea83681de
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 05:46:53 -0700

    Updated navbar to say Sponsor instead of donate

--- client/src/components/Navbar/navbarData.js
+++ client/src/components/Navbar/navbarData.js
@@ -30,8 +30,8 @@ export const NavbarData = [
         cName: 'nav-text'
     },
     {
-        title: 'Donate',
-        path: '/donate',
+        title: 'Sponsor',
+        path: '/sponsor',
         icon: <MdIcons.MdOutlineAttachMoney />,
         cName: 'nav-text'
     },
