fe7eaa48c507473a3563de50249fd803b1b4a451
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 05:59:22 -0700

    Set up Order and Modal products, added Order schema to User

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Workout = require('./Workout');
 const Exercise = require('./Exercise');
+const Order = require('./Order');
+const Product = require('./Product');
 
-module.exports = { User, Exercise, Workout };
+module.exports = { User, Exercise, Workout, Order, Product };
 
