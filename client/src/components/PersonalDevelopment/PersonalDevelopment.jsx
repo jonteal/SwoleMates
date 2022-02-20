fb54411486a0328e73c7a0e82f21721d2953d122
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 13:19:36 -0700

    Srtting up why card

--- client/src/components/PersonalDevelopment/PersonalDevelopment.jsx
+++ client/src/components/PersonalDevelopment/PersonalDevelopment.jsx
@@ -1,45 +1,38 @@
 import React, { useState } from "react";
-import './personalDevelopment.css';
+import "./personalDevelopment.css";
 
 const PersonalDevelopment = () => {
+  const [inputWhy, setWhy] = useState("");
 
-    const [text, setText] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e.target.value);
+    setWhy(e.target.value);
+    console.log(inputWhy)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const body = { text }
 
-        // Can we use a POST fetch request and post this user input to our "HomePage"?
-        // Location of fetch request can be where it will populate on homepage. Something like 
-        // 'http://localhost:8000/goals' or something
-        fetch('', {         
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body)
-        }).then(() => {
-            console.log('new personal development goal added!')
-        })
+    // Can we use a POST fetch request and post this user input to our "HomePage"?
+    // Location of fetch request can be where it will populate on homepage. Something like
+    // 'http://localhost:8000/goals' or something
 
-    }
-    return (
-        <div className="dev-container filter drop-shadow-lg">
-            <form className="dev-form" onSubmit={handleSubmit}>
-                <textarea
-                    className="dev-input"
-                    rows="2"
-                    cols="30"
-                    placeholder="What is your WHY?"
-                    type="text"
-                    required
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                />
-                <button className="dev-btn filter drop-shadow-lg">
-                    Save
-                </button>
-            </form>
-        </div>
-    )
-}
+  };
+  return (
+    <div className="dev-container filter drop-shadow-lg">
+      <form className="dev-form" onSubmit={handleSubmit}>
+        <textarea
+          className="dev-input"
+          rows="2"
+          cols="30"
+          placeholder="What is your WHY?"
+          type="text"
+          required
+          onChange={handleSubmit}
+          value={inputWhy}
+        />
+        <button className="dev-btn filter drop-shadow-lg" onChange={(e) => setWhy(e)}>Save</button>
+      </form>
+    </div>
+  );
+};
 
 export default PersonalDevelopment;
