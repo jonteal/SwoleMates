7cefabb6735b1565520b2687fcf0a25c3f04a1b5
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-20 15:35:10 -0600

    added useState, useMutation, and useEffect logic to SearchResults.jsx

--- client/src/components/SearchResults/SearchResults.jsx
+++ client/src/components/SearchResults/SearchResults.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './searchResults.css';
-
+import { ADD_FRIEND } from './utils/mutations';
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
 
 const SearchResults = () => {
+
+    // create state for holding returned searched friend data
+    const [searchedFriends, setSearchedFriends] = useState([]);
+
+    // create state for holding our search field data
+    const  [searchInput, setSearchInput] = useState('');
+
+    // create state to hold added friends values
+    const [addedFriendsIds, setAddedFriendsIds] = useState(getAddedFriendsIds());
+
+    // create mutation to add a friend to friends values
+    const [addFriend, {error}] = useMutation(ADD_FRIEND);
+
+    // set up useEffect hook to save 'addedFriendsIds' list to localStorage on component unmount
+    useEffect(() => {
+        return () => addedFriendsIds(addedFriendsIds);
+    });
+
+
+
     return(
         <>
 
