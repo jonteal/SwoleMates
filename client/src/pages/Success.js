9c5f5e7fd100e7db1b11b0fa434b07ef4f3b6122
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-20 07:36:57 -0700

    Ensuring up to date with main

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -22,7 +22,7 @@ function Success() {
       }
 
       setTimeout(() => {
-        window.location.assign('/');
+        window.location.assign('/home');
       }, 3000);
     }
 
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -9,7 +9,7 @@ const {
 const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 const { Error } = require("mongoose");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const resolvers = {
   Query: {
