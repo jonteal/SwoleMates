a71e7de93858646cb794025156811ab03da52a2a
NinjaStep <r.paragas@outlook.com>
2022-02-20 20:14:16 -0700

    added database pull to display weight on frontend

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,9 @@
 import { gql } from '@apollo/client';
 
 export const GET_WEIGHT = gql`
-    query me {
-        me {
-            _id
-            username
-            email
+    query getUser {
+      getUser {
             weight
-            height
-            age
-            sex
-            goal
         }
     }
 `;
