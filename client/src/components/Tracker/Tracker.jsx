bf70146cf690c92a7848a4fc4d6e23d18f0c70da
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-21 20:20:20 -0700

    Added log workout btn

--- client/src/components/Tracker/Tracker.jsx
+++ client/src/components/Tracker/Tracker.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import './tracker.css'
-import LogSwole from './Exercise'
-import DisplaySwole from './Workout'
+import "./tracker.css";
+import LogSwole from "./Exercise";
+import DisplaySwole from "./Workout";
 
 const Tracker = () => {
-    return (
-        <>
-        <div className="trackerContainer">
-            <DisplaySwole />
-        </div>
-        {/* <LogSwole /> */}
-        </>
+  return (
+    <>
+      <div className="trackerContainer">
+        <DisplaySwole />
+      </div>
 
-    )
-}
+      <div className="workoutBtn">
+        <button className="button-30" role="button">
+          + Log Workout
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default Tracker;
-
