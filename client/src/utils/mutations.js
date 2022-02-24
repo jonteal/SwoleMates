5e27874ae11b6674c0ba0687b928c419f197fa5f
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-23 19:16:43 -0700

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -156,7 +156,6 @@ export const FOLLOW_UNFOLLOW = gql`
   mutation followUnfollow($otherUserId: ID!) {
     followUnfollow(otherUserId: $otherUserId) {
       _id
-      username
       following {
         _id
       }
