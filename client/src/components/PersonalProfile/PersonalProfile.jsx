542879fdb74c45ed667e4ddf5e77d98cc288b0c6
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-25 22:00:03 -0700

    Added redirect to

--- client/src/components/PersonalProfile/PersonalProfile.jsx
+++ client/src/components/PersonalProfile/PersonalProfile.jsx
@@ -26,6 +26,11 @@ const PersonalProfile = () => {
     return <p>Patience, mate...</p>;
   }
 
+  const handleFindMates = (e) => {
+      e.preventDefault();
+      window.location.assign('/search');
+  }
+
   const followers = user?.followers || [];
   const following = user?.following || [];
 
@@ -51,7 +56,7 @@ const PersonalProfile = () => {
                 <Link to="/followers">Followers</Link>
                 <Link to="/following">Following</Link>
               </div>
-              <button class="matesBtn pt-7">Search for Mates</button>
+              <button class="matesBtn pt-7" onClick={handleFindMates}>Search for Mates</button>
             </div>
             </div>
           </div>
